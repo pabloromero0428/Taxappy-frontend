56d392b27b2045327f47a8130663965c693220b9
Juan Pablo Romero <pablo.romero2840@gmail.com>
2019-09-20 18:53:18 -0500

    Intallacion de vuetify

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
